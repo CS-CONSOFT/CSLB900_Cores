216ff0850e246a2055b41a9cd26f1b7947ee2817
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-07-25 14:55:35 -0300

    Update Dto_CreateUpdateSY001.cs

--- CSCore.Application.Dto/Dtos/Sistema/SY001/SY001/Dto_CreateUpdateSY001.cs
+++ CSCore.Application.Dto/Dtos/Sistema/SY001/SY001/Dto_CreateUpdateSY001.cs
@@ -42,7 +42,7 @@ namespace CSSY103.C82Application.Dto.SY001.SY001
 
         public string? Sy001Celular { get; set; }
 
-        //liha
+        //liha sfdfsdf
 
         //public Csicp_Sy809Idioma? Sy001Idioma { get; set; }
     }
