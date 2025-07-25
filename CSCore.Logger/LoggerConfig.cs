30490ff5a6bee13d9348d31b8a87d564bcc9a6a3
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-07-25 10:22:09 -0300

    feat

---
+++ CSCore.Logger/LoggerConfig.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace CS_Logs;
+
+public class LoggerConfig
+{
+    public static void ConfigurarLogger()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/log-.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Log.Information("Logger configurado a partir de MinhaBibliotecaLogs!");
+    }
+}
