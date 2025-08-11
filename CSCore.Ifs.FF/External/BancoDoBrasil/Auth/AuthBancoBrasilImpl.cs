152cc1df73ea23c3fbe8e83dfc8e6e71f4cdeeea
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-08-11 17:29:04 -0300

    feat: Implementa serviço do Banco do Brasil e refatora código
    Adicionadas novas referências e namespaces para o serviço do Banco do Brasil, incluindo a criação da classe abstrata `BancoBrasilAbstractClass` e a implementação das classes `EnvioTituloBancoBrasil` e `RetornarListaBoleto`.
    
    A interface `IAuthBancoBrasil` foi criada para gerenciar a autenticação, com a implementação `AuthBancoBrasilImpl`. O código foi refatorado para melhorar a estrutura e a legibilidade, garantindo consistência nas operações relacionadas ao Banco do Brasil.

---
+++ CSCore.Ifs.FF/External/BancoDoBrasil/Auth/AuthBancoBrasilImpl.cs
@@ -0,0 +1,24 @@
+﻿using CSCore.Ifs.FF.External;
+using CSCore.Ifs.FF.External.Parametros;
+using CSLB900.MSTools.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.External.BancoDoBrasil.Auth
+{
+    public class AuthBancoBrasilImpl(IRefitBancoBrasil refitBancoBrasil) : IAuthBancoBrasil
+    {
+        private readonly IRefitBancoBrasil _refitBancoBrasil = refitBancoBrasil;
+        public async Task<ReturnPostLogin> CS51_Auth_BancoBrasil(string in_tokenAutenticacao)
+        {
+            var apiAuthBBResponse = await _refitBancoBrasil.Auth(Authorization: in_tokenAutenticacao);
+            if (!apiAuthBBResponse.IsSuccessful)
+                throw new Exception("Erro no login: " + HandlerExceptionMessage.CreateExceptionMessage(apiAuthBBResponse.Error));
+            ReturnPostLogin retornoPostLogin = apiAuthBBResponse.Content;
+            return retornoPostLogin;
+        }
+    }
+}
