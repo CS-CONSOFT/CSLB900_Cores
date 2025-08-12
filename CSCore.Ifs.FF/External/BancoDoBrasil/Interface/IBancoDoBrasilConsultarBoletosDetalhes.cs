5e596c7edd43312b183e03c693c72dc85f546e95
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-08-12 17:41:21 -0300

    feat: acabando bb

---
+++ CSCore.Ifs.FF/External/BancoDoBrasil/Interface/IBancoDoBrasilConsultarBoletosDetalhes.cs
@@ -0,0 +1,15 @@
+﻿using CSCore.Ifs.FF.External.Retornos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.External.BancoDoBrasil.Interface
+{
+    public interface IBancoDoBrasilConsultarBoletosDetalhes : IBancoDoBrasilAuth
+    {
+        Task<RetornoConsultarBoleto> ConsultarBoletosDetalhes(string gwdevappkey,
+            string estabAuthToken, string idBoleto, int in_numeroConvenio);
+    }
+}
