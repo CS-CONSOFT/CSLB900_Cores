bed55c1b8ce8b21c17a4f0e865788bb2b54a696b
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-08-13 15:45:26 -0300

    Atualiza subprojeto e permite valores nulos em Data
    Alterado o commit do subprojeto para `c2e34d7838cb6b35a68b2d073e2e2582ad58c0a5-dirty`.
    Modificada a propriedade `Data` na classe `DtoApiResponse<T>` para permitir valores nulos, alterando de `public T Data { get; set; }` para `public T? Data { get; set; }`.

--- CSCompartilhado.Exception/DtoApiResponse.cs
+++ CSCompartilhado.Exception/DtoApiResponse.cs
@@ -5,7 +5,7 @@
         public string TraceID { get; set; }
         public bool Success { get; set; }
         public string Message { get; set; } = string.Empty;
-        public T Data { get; set; }
+        public T? Data { get; set; }
         public string? CaminhoEndpoint { get; set; }
         public object? HeadersRequisicao { get; set; }
         
