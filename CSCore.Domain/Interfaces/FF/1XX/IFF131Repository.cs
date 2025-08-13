be232fd6e56bb5e620b9ca61aa36911fba071f13
JardelB <jardelbarros@cs-consoft.com.br>
2025-08-13 11:49:22 -0300

    feat: Adiciona repositório FF131 e sua interface
    Modifica o parâmetro do método `Delete` em `FF018Controller.cs` para `in_ff018Id` e atualiza a chamada ao serviço de deleção, pois estava com nomenclartura do Id da ff017.
    
    Cria a interface `IFF131Repository` em `IFF131Repository.cs` com métodos assíncronos para listar e buscar registros.
    
    Implementa a lógica de acesso a dados em `FF131RepositoryImpl.cs`, utilizando `Entity Framework` para consultas assíncronas.

---
+++ CSCore.Domain/Interfaces/FF/1XX/IFF131Repository.cs
@@ -0,0 +1,25 @@
+﻿using CSCore.Domain.CS_Models.CSICP_FF;
+using CSCore.Domain.Interfaces.V2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Domain.Interfaces.FF._1XX
+{
+    public interface IFF131Repository : IRepositorioBase<CSICP_FF131>
+    {
+        Task<(List<CSICP_FF131>, int)> GetListAsync(
+            int in_tenant,
+            int in_pageNumber,
+            int in_pageSize,
+            string? in_estabId,
+            DateTime? in_periodoInicial,
+            DateTime? in_periodoFinal,
+            string? in_protocolo,
+            string? in_nomeContaCliente);
+
+        Task<CSICP_FF131?> GetByIdAsync(int in_tenant, long in_ff131Id);
+    }
+}
