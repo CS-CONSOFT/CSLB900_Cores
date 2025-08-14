a6961ef49f14e7dac4b952e771e491a6bb4cefb7
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-08-14 09:32:21 -0300

    fix: Implementa exceções e atualiza DtoApiResponse
    Adiciona exceções nos métodos `Create` e `GetList` do
    arquivo `BB005Controller.cs` para indicar que os endpoints
    ainda não foram implementados.
    
    Atualiza as propriedades `Success`, `Message` e `TraceID`
    no arquivo `DtoApiResponse.cs` para serem do tipo nullable,
    permitindo maior flexibilidade nas respostas da API.
    
    Atualiza o commit do subprojeto `CSLB900_Cores` para
    refletir alterações não confirmadas.

--- CSCompartilhado.Exception/DtoApiResponse.cs
+++ CSCompartilhado.Exception/DtoApiResponse.cs
@@ -2,11 +2,11 @@
 {
     public class DtoApiResponse<T>
     {
-        public bool Success { get; set; }
-        public string Message { get; set; } = string.Empty;
+        public bool? Success { get; set; }
+        public string? Message { get; set; } = string.Empty;
         public T? Data { get; set; }
 
-        public string TraceID { get; set; }
+        public string? TraceID { get; set; }
         public string? CaminhoEndpoint { get; set; }
         public object? HeadersRequisicao { get; set; }
         
