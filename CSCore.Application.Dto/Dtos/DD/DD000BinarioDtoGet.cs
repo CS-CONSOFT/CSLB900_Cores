5ec8b96c638df6d311478f68df72fbb81059bf21
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-08-21 08:57:19 -0300

    feat: Atualiza serviços e repositórios para certificados
    Implementa nova interface IDD000Service e suas classes associadas para manipulação de certificados e senhas. Refatora DD060ServiceImpl.cs para usar a namespace DD._06X. Remove métodos obsoletos e simplifica o código em IDD040Repository.cs e DD040RepositoryImpl.cs.

---
+++ CSCore.Application.Dto/Dtos/DD/DD000BinarioDtoGet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Application.Dto.Dtos.DD
+{
+    public class DD000BinarioDtoGet
+    {
+        public byte[]? Dd000ArquivoBinarioDigital { get; set; }
+        public string? Dd000Senhacertdigital { get; set; }
+    }
+}
---
+++ CSCore.Ifs.EnviaNFeHercules/Repository/DD000/IDD000Repository].cs
@@ -0,0 +1,14 @@
+﻿using CSCore.Domain.CS_Models.CSICP_DD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.EnviaNFeHercules.Repository.DD000
+{
+   public interface IDD000Repository
+    {
+        Task<CSICP_DD000> GetCertificadoESenha(int in_tenant, string in_estabID);
+    }
+}
