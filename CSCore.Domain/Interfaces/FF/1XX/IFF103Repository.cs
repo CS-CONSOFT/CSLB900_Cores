4381fffa6c3f029862b65622889f93eb8cb77294
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-08-25 15:51:17 -0300

    feat: Adiciona suporte ao serviço FF103 e operações CRUD
    Implementa a interface IFF103Service e sua implementação FF103ServiceImpl, além do repositório FF103Repository para manipulação de dados do FF103. Atualiza a unidade de trabalho IUnityOfWorkFF para incluir o novo serviço. Cria o controlador FF103Controller para expor endpoints RESTful para operações CRUD. Introduz classes DTO para transferência de dados e implementa mapeamento entre entidades e DTOs. O repositório FF103RepositoryImpl é adicionado para interações com o banco de dados usando Entity Framework. O código foi organizado para seguir melhores práticas de separação de preocupações.

---
+++ CSCore.Domain/Interfaces/FF/1XX/IFF103Repository.cs
@@ -0,0 +1,12 @@
+﻿using CSCore.Domain.CS_Models.CSICP_FF;
+using CSCore.Domain.Interfaces.V2;
+
+namespace CSCore.Domain.Interfaces.FF._1XX
+{
+    public interface IFF103Repository : IRepositorioBase<CSICP_FF103>
+    {
+        Task<(List<CSICP_FF103>, int)> GetListAsync(int in_tenant, int in_pageNumber, int in_pageSize);
+
+        Task<CSICP_FF103> GetByIdAsync(int in_tenant, string in_ff103Id);
+    }
+}
