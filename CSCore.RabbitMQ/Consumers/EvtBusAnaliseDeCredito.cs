14b8a90bc6c43e27d3d60d18d3698b2c14f7c9a1
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-08-26 08:53:54 -0300

    feat: Adiciona suporte a notificações de análise de crédito
    Novas dependências para RabbitMQ e SignalR foram adicionadas ao arquivo `EvtBusAnaliseDeCredito.cs`. A classe `EvtBusAnaliseDeCredito` agora inclui um parâmetro `IHubContext<HubNotification>` para enviar mensagens a grupos de clientes. Um bloco `try-catch` foi implementado no método `Consume` para registrar mensagens e notificar clientes sobre o sucesso ou falha da análise de crédito.
    
    Além disso, foi adicionada a constante `ANALISE_CREDITO` em `HubGroupNames.cs` e a propriedade `in_usuarioID` na classe `Rbt_CS_AnaliseCredito` para armazenar a identificação do usuário.

--- CSCore.RabbitMQ/Consumers/EvtBusAnaliseDeCredito.cs
+++ CSCore.RabbitMQ/Consumers/EvtBusAnaliseDeCredito.cs
@@ -1,24 +1,51 @@
 ﻿using CSCore.Ifs.AnaliseDeCredito.AnaliseCredito;
+using CSCore.RabbitMQ.Hub;
+using CSCore.RabbitMQ.Hub.Ax;
 using CSCore.RabbitMQ.PublishObjetos;
+using CSLB900.MSTools.Util;
 using MassTransit;
+using Microsoft.AspNetCore.SignalR;
 using Serilog;
 
 namespace CSCore.RabbitMQ.Bus
 {
-    public class EvtBusAnaliseDeCredito(CreditoSemScore creditoSemScore) : IConsumer<Rbt_CS_AnaliseCredito>
+    public class EvtBusAnaliseDeCredito(CreditoSemScore creditoSemScore, IHubContext<HubNotification> hubContext) : IConsumer<Rbt_CS_AnaliseCredito>
     {
         private CreditoSemScore _creditoSemScore = creditoSemScore;
+        private readonly IHubContext<HubNotification> _hubContext = hubContext;
         public async Task Consume(ConsumeContext<Rbt_CS_AnaliseCredito> context)
         {
-            Log.Information("RabbitMQ: Mensagem recebida no consumer {Consumer} às {Data}. Tipo da mensagem: {MessageType}. Conteúdo: {@Message}",
-                this.GetType().Name,
-                DateTime.UtcNow.ToLocalTime(),
-                context.Message.GetType().Name,
-                context.Message);
-            await _creditoSemScore.ExecutarAnaliseCredito(
-                context.Message.in_CalcularScoreClearsale,
-                context.Message.in_tenantID,
-                context.Message.in_conta_ID);
+            try
+            {
+                Log.Information("RabbitMQ: Mensagem recebida no consumer {Consumer} às {Data}. Tipo da mensagem: {MessageType}. Conteúdo: {@Message}",
+                    this.GetType().Name,
+                    DateTime.UtcNow.ToLocalTime(),
+                    context.Message.GetType().Name,
+                    context.Message);
+                await _creditoSemScore.ExecutarAnaliseCredito(
+                    context.Message.in_CalcularScoreClearsale,
+                    context.Message.in_tenantID,
+                    context.Message.in_conta_ID);
+
+                await _hubContext.Clients.Group("analise-de-credito-" + context.Message.in_usuarioID)
+                   .SendAsync(HubMethodNames.ANALISE_CREDITO, new
+                   {
+                       Success = true,
+                       Message = "Sucesso ao realizar analise de credito",
+                       Timestamp = DateTime.UtcNow
+                   });
+            }
+            catch (Exception ex)
+            {
+                await _hubContext.Clients.Group("analise-de-credito-" + context.Message.in_usuarioID)
+                .SendAsync(HubMethodNames.ANALISE_CREDITO, new
+                {
+                    Success = false,
+                    Message = "Falha ao realizar analise de credito: " + HandlerExceptionMessage.CreateExceptionMessage(ex),
+                    Timestamp = DateTime.UtcNow
+                });
+                throw new Exception(HandlerExceptionMessage.CreateExceptionMessage(ex));
+            }
         }
     }
 }
