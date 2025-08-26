cb3c22ce140cdb8d965121ed44e19ca42b3c2791
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-08-26 08:56:45 -0300

    feat: Adiciona suporte para geração de protocolos CI
    Inclui a propriedade `InUsuarioID` na classe `DtoGerarProtocoloCI` para identificar o usuário. A classe `EvtBusGeraProtocoloCI` agora possui um contexto de hub e um bloco `try-catch` no método `Consume` para enviar mensagens de sucesso ou falha via SignalR. Adicionada a constante `GERA_PROTOCOLO_CI` em `HubMethodNames` para identificar a operação de geração de protocolo.

--- CSCore.Application.Dto/Dtos/EvtRabbitMQ/DtoGerarProtocoloCI.cs
+++ CSCore.Application.Dto/Dtos/EvtRabbitMQ/DtoGerarProtocoloCI.cs
@@ -5,5 +5,6 @@
         public string EmpresaID { get; set; } = string.Empty;
         public string TextName { get; set; } = string.Empty;
         public string Arquivo { get; set; } = string.Empty;
+        public string InUsuarioID { get; set; } = string.Empty;
     }
 }
