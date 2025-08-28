1f89c042edfb1923f675d8cb0497ff86894daaff
JardelB <jardelbarros@cs-consoft.com.br>
2025-08-28 16:31:42 -0300

    feat: Ajusta tipos de dados de ID para long em interfaces
    As mudanças realizadas no código envolvem a alteração dos tipos de dados dos parâmetros de métodos em várias interfaces e implementações, especificamente de `string` para `long` para os IDs. Isso foi feito para garantir que os IDs sejam tratados como números longos, o que pode ser mais apropriado dependendo do contexto de uso. Além disso, houve a remoção do uso de `string?` (que permite valores nulos) para `string` em alguns métodos, o que implica que agora esses métodos não aceitam mais valores nulos para esses parâmetros. As mudanças foram aplicadas em várias classes, incluindo `IFF006Service`, `FF006ServiceImpl`, `IFF006Repository`, e `FF006RepositoryImpl`, refletindo uma consistência no tratamento de IDs em todo o sistema. Também houve uma atualização na mensagem de exceção lançada em um dos métodos para melhorar a clareza da mensagem de erro.

--- CSCore.Domain/Interfaces/FF/00X/IFF006Repository.cs
+++ CSCore.Domain/Interfaces/FF/00X/IFF006Repository.cs
@@ -10,7 +10,7 @@ namespace CSCore.Domain.Interfaces.FF._00X
 {
     public interface IFF006Repository : IRepositorioBase<CSICP_FF006>
     {
-        Task<(List<CSICP_FF006>, int)> GetListAsync(int tenant, string? ff102Id, int page, int pageSize);
-        Task<CSICP_FF006?> GetByIdAsync(int tenant, string id);
+        Task<(List<CSICP_FF006>, int)> GetListAsync(int tenant, string ff102Id, int page, int pageSize);
+        Task<CSICP_FF006?> GetByIdAsync(int tenant, long id);
     }
 }
