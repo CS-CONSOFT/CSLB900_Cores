fce01498d5b75ac0feb764a18fa230405b9716b5
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-08-29 16:04:13 -0300

    Adicionar cálculo de valores de inventário
    Novos métodos e interfaces foram implementados para calcular valores de inventário na aplicação. O método `CalcularValoresInventario` foi adicionado à interface `IGG032Service` e implementado na classe `GG032ServiceImpl`. Foi criada a classe `CalculaValoresInventarioImpl`, que contém a lógica de cálculo baseada em dados do banco. As dependências e injeções de serviços foram atualizadas para suportar essa nova funcionalidade. O controlador `GG032Controller` agora expõe um novo endpoint HTTP PATCH para calcular os valores de inventário via requisição.

---
+++ CSCore.Ifs.GG/Repository/GG/03X/GG032/CalculaValoresInventario/ICalculaValoresInventario.cs
@@ -0,0 +1,7 @@
+﻿namespace CSCore.Ifs.GG.Repository.GG._03X.GG032.CalculaValoresInventario
+{
+    public interface ICalculaValoresInventario
+    {
+        Task<string> Calcular(string InGG032ID, int InTenantID);
+    }
+}
