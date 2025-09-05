fdfcfa43c0ec9aa578a19b87a3e112d20097af11
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-09-05 10:08:22 -0300

    Adiciona estratégias de incremento de data
    Implementa a interface IIncrementarDataStrategy com as classes
    IncrementarDataPorDiaStrategy e IncrementarDataPorMesStrategy.
    A lógica de incremento de data na classe
    ProcessarParcelasTipoParcelaDiasOuMes foi atualizada para
    utilizar essas novas estratégias, melhorando a flexibilidade
    e manutenção do código.

---
+++ CSCore.Ifs.FF/Repository/Processos/CS_Renegociacao_Calc_Titulos/Strategy/CalculoAdicaoDataStrategy/IIncrementarDataStrategy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.Repository.Processos.CS_Renegociacao_Calc_Titulos.Strategy.CalculoAdicaoDataStrategy
+{
+    public interface IIncrementarDataStrategy
+    {
+        DateTime IncrementarData(DateTime Data, int intervaloParaAdicao);
+    }
+}
---
+++ CSCore.Ifs.FF/Repository/Processos/CS_Renegociacao_Calc_Titulos/Strategy/CalculoAdicaoDataStrategy/IncrementarDataPorMesStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.Repository.Processos.CS_Renegociacao_Calc_Titulos.Strategy.CalculoAdicaoDataStrategy
+{
+    public class IncrementarDataPorMesStrategy : IIncrementarDataStrategy
+    {
+        public DateTime IncrementarData(DateTime Data, int intervaloParaAdicao)
+        {
+            return Data.AddMonths(intervaloParaAdicao);
+        }
+    }
+}
