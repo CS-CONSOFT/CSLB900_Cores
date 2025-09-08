51cc490aec49df13d7f3e0a42fbd4465c8bcea95
JardelB <jardelbarros@cs-consoft.com.br>
2025-09-08 19:30:22 -0300

    feat: Adiciona repositórios e serviços para AA143 e AA144
    Implementa novos repositórios, serviços e controladores para as entidades AA143 e AA144. Atualiza `InjecaoRepositorios.cs` e `IAAUnityOfWork.cs` para suportar a injeção de dependências. Cria controladores para expor endpoints de listagem, além de interfaces e implementações de serviços para manipulação de dados. Adiciona mapeadores e classes DTO para facilitar a conversão entre entidades e dados transferidos.

---
+++ CSCore.Application.Dto/Dtos/Basico_AA/AA1XX/AA143/DtoGetAA143.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace CSCore.Application.Dto.Dtos.Basico_AA.AA1XX.AA143
+{
+    public class DtoGetAA143
+    {
+        public string Id { get; set; } = null!;
+
+        public string? Aa043Artigo { get; set; }
+
+        public string? Aa043LcRedacao { get; set; }
+
+        public string? Aa043Ec { get; set; }
+    }
+}
