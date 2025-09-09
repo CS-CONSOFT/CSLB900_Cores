bc70bc3cf081b7fb652febc04d609b0ff2bcd1b9
JardelB <jardelbarros@cs-consoft.com.br>
2025-09-09 14:39:45 -0300

    refatoracao: Remover parâmetro in_estabID de métodos do IFF011
    Alterações realizadas nos arquivos de código incluem a remoção do parâmetro `in_estabID` de vários métodos no serviço `IFF011Service`, sua implementação `IFF011ServiceImpl`, e no controlador `FF011Controller`. Essa mudança simplifica a interface e a lógica de recuperação de listas, tornando a API mais flexível e alinhada com novas exigências de negócio. A lógica de filtragem no repositório `IFF011Repository` também foi ajustada para refletir essa remoção, indicando uma possível mudança na organização e acesso aos dados.

--- CSCore.Domain/Interfaces/FF/01X/IFF011Repository.cs
+++ CSCore.Domain/Interfaces/FF/01X/IFF011Repository.cs
@@ -7,7 +7,7 @@ namespace CSCore.Domain.Interfaces.FF._01X
     public interface IFF011Repository : IRepositorioBase<CSICP_FF011>
     {
         Task<(List<RepoDtoCSICP_FF011>, int)> GetListAsync(int in_tenantID, int in_pageNumber, int in_pageSize,
-            string in_estabID, string? in_tipoCobrancaID, string? in_categoriaID);
+             string? in_tipoCobrancaID, string? in_categoriaID);
         Task<RepoDtoCSICP_FF011?> GetByIdAsync(int in_tenantID, string in_ff011ID);
     }
 }
