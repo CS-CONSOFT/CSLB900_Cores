f299a775e61f4f600999e973d060ca61539acbc5
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-09-12 14:15:55 -0300

    Atualiza subprojeto e adiciona referência ao EF Core
    Atualização do subprojeto `CSLB900_Cores` para o commit
    `b5e9110987565229c9b7d6eb3ff719b7154b10c6-dirty`.
    
    Adicionada referência ao namespace `Microsoft.EntityFrameworkCore`
    no arquivo `IncludeNavFF002Motivo.cs`, possibilitando o uso de
    funcionalidades do Entity Framework Core.

--- CSCore.Ifs.FF/Repository/FF1XX/FF125/IncludesFF125List/IncludeNavFF002Motivo.cs
+++ CSCore.Ifs.FF/Repository/FF1XX/FF125/IncludesFF125List/IncludeNavFF002Motivo.cs
@@ -1,5 +1,6 @@
 ﻿using CSCore.Domain.CS_Models.CSICP_FF;
 using CSCore.Domain.Interfaces.V2;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
