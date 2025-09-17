bccbbf98d5732af31572b00a3a4d30b01ee38ea1
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-09-17 18:16:11 -0300

    Refatoração e introdução de Unit of Work V2
    - Adicionada a interface IUnitOfWorkV2 e sua implementação UnitOfWorkV2Impl para centralizar transações e repositórios.
    - Refatorado FF127ServiceImpl e controladores para usar IUnitOfWorkV2, reduzindo acoplamento e melhorando consistência transacional.
    - Criados novos serviços (CS_SPCServiceImpl) e controladores (CS_RegistrarSPCController) para registro no SPC.
    - Introduzida nova base de repositórios genéricos (RepositorioBaseImplV2) com suporte a operações CRUD.
    - Ajustadas rotas de APIs para maior clareza e organização.
    - Adicionadas constantes e classes auxiliares para padronizar retornos e parâmetros.
    - Melhorias gerais em performance, clareza e documentação do código.

---
+++ CSCore.Ifs.FF/Repository/Processos/Agenda de Cobranca/ISPCRepository.cs
@@ -0,0 +1,14 @@
+﻿using CSCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.Repository.Processos.Agenda_de_Cobranca
+{
+    public interface ISPCRepository
+    {
+        Task<RetornoPadraoProcessosVoid> AtualizaRegistroSPC(PrmRegistraSPC prm);
+    }
+}
---
+++ CSCore.Ifs.FF/Repository/Processos/Agenda de Cobranca/PrmRegistraSPC.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.Repository.Processos.Agenda_de_Cobranca
+{
+    public class PrmRegistraSPC
+    {
+        public int InTenantId { get; set; }
+        public string InFF126_ID { get; set; } = string.Empty;
+        public int InStIDCsicp_bb012_SitCta_CIS { get; set; }
+        public DateTime InFf126_DtRegistro { get; set; }
+        public string InFf126_Mensagem { get; set; } = string.Empty;
+        public string InSY001_ID { get; set; } = string.Empty;
+
+        public PrmRegistraSPC(int inTenantId, string inFF126_ID, int inStIDCsicp_bb012_SitCta_CIS, DateTime inFf126_DtRegistro, string inFf126_Mensagem, string inSY001_ID)
+        {
+            InTenantId = inTenantId;
+            InFF126_ID = inFF126_ID;
+            InStIDCsicp_bb012_SitCta_CIS = inStIDCsicp_bb012_SitCta_CIS;
+            InFf126_DtRegistro = inFf126_DtRegistro;
+            InFf126_Mensagem = inFf126_Mensagem;
+            InSY001_ID = inSY001_ID;
+        }
+    }
+}
