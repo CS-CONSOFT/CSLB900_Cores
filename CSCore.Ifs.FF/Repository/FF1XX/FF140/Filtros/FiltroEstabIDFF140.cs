3ab2c8263ae26edf8410dbb8328428c109bd8822
JardelB <jardelbarros@cs-consoft.com.br>
2025-09-29 18:44:37 -0300

    feat: Criando API Crud FF140
    Implementa a classe Filtros da FF140. O método `Apply` permite aplicar um filtro em uma consulta `IQueryable<CSICP_FF140>`, com verificação não nulo ou vazio.
    Cria service da FF140 com GetByID, GetList e regras no POST.

---
+++ CSCore.Ifs.FF/Repository/FF1XX/FF140/Filtros/FiltroEstabIDFF140.cs
@@ -0,0 +1,22 @@
+﻿using CSCore.Domain.CS_Models.CSICP_FF;
+
+namespace CSCore.Ifs.FF.Repository.FF1XX.FF140.Filtros
+{
+    internal class FiltroEstabIDFF140
+    {
+        private readonly string? _estabID;
+        public FiltroEstabIDFF140(string? estabID)
+        {
+            _estabID = estabID;
+        }
+
+        public IQueryable<CSICP_FF140> Apply(IQueryable<CSICP_FF140> query)
+        {
+            if (!string.IsNullOrEmpty(_estabID))
+            {
+                //query = query.Where(e => e.Ff140Empresaid == _estabID);
+            }
+            return query;
+        }
+    }
+}
