65dd47fef8adad22dbbcce745f1d2fa7b13d1b44
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-10-08 11:45:54 -0300

    feat: finalizando #21

---
+++ CSCore.Domain/Interfaces/FF/1XX/FF102/PR21_ListaTitulosGeradosQualquerOrigem/IPR21_FF102ListaTitulosGeradosQualquerOrigem.cs
@@ -0,0 +1,15 @@
+﻿using CSCore.Domain.CS_Models.CSICP_FF;
+using CSCore.Ifs.FF.Repository.FF1XX.FF102.PR21_ListaTitulosGeradosQualquerOrigem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Domain.Interfaces.FF._1XX.FF102.PR21_ListaTitulosGeradosQualquerOrigem
+{
+    public interface IPR21_FF102ListaTitulosGeradosQualquerOrigem
+    {
+        Task<List<CSICP_FF102>> Execute(PR21_EnumTipoOrigem pR21_EnumTipo, string IdControle, int in_tenant);
+    }
+}
---
+++ CSCore.Ifs.FF/Repository/FF1XX/FF102/PR21_ListaTitulosGeradosQualquerOrigem/PR21_Strategy/PR21_GetQueryImplHandler.cs
@@ -0,0 +1,25 @@
+﻿using CSCore.Ifs.CS_Context;
+using CSCore.Ifs.FF.Repository.FF1XX.FF102.PR21_ListaTitulosGeradosQualquerOrigem;
+
+namespace CSCore.Ifs.FF.Repository.FF1XX.FF102.ListaTitulosGeradosQualquerOrigem.Strategy
+{
+    public class PR21_GetQueryImplHandler
+    {
+        public static PR21_IQueryImpl GetQueryImpl(
+            PR21_EnumTipoOrigem enumtp,
+            AppDbContext appDbContext,
+            string IdControle
+            )
+        {
+            return enumtp switch
+            {
+                PR21_EnumTipoOrigem.csicp_dd040 => new PR21_DD040QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_cc030 => new PR21_CC030QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_cc040 => new PR21_CC040QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_bf010 => new PR21_BF010QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_ff040 => new PR21_FF040QueryImpl(appDbContext, IdControle),
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
