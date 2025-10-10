304197519e228743fa3f9b3a2b983cafaa7be512
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-10-10 08:18:26 -0300

    Atualiza rotas e adiciona novas estratégias PR21
    - Alterada a rota do método `GetById` no `FF000Controller.cs` de `[HttpGet("{id}")]` para `[HttpGet("by-id")]`.
    - Atualizado o commit do subprojeto `CSLB900_Cores` para `abb379913ed1021002778f95ede533101ba38c82-dirty`.
    - Adicionada referência ao namespace `PR21_Strategy` no arquivo `PR21_GetQueryImplHandler.cs`.
    - Incluídas novas implementações de estratégias no enum `PR21_EnumTipoOrigem`:
      - `csicp_ff017` com `PR21_FF017QueryImpl`.
      - `csicp_dd190` com `PR21_DD190QueryImpl`.
      - `csicp_ge010` com `PR21_GE010QueryImpl`.
      - `csicp_ff140` com `PR21_FF140QueryImpl`.

--- CSCore.Ifs.FF/Repository/FF1XX/FF102/PR21_ListaTitulosGeradosQualquerOrigem/PR21_Strategy/PR21_GetQueryImplHandler.cs
+++ CSCore.Ifs.FF/Repository/FF1XX/FF102/PR21_ListaTitulosGeradosQualquerOrigem/PR21_Strategy/PR21_GetQueryImplHandler.cs
@@ -1,5 +1,6 @@
 ﻿using CSCore.Ifs.CS_Context;
 using CSCore.Ifs.FF.Repository.FF1XX.FF102.PR21_ListaTitulosGeradosQualquerOrigem;
+using CSCore.Ifs.FF.Repository.FF1XX.FF102.PR21_ListaTitulosGeradosQualquerOrigem.PR21_Strategy;
 
 namespace CSCore.Ifs.FF.Repository.FF1XX.FF102.ListaTitulosGeradosQualquerOrigem.Strategy
 {
@@ -18,6 +19,10 @@ namespace CSCore.Ifs.FF.Repository.FF1XX.FF102.ListaTitulosGeradosQualquerOrigem
                 PR21_EnumTipoOrigem.csicp_cc040 => new PR21_CC040QueryImpl(appDbContext, IdControle),
                 PR21_EnumTipoOrigem.csicp_bf010 => new PR21_BF010QueryImpl(appDbContext, IdControle),
                 PR21_EnumTipoOrigem.csicp_ff040 => new PR21_FF040QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_ff017 => new PR21_FF017QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_dd190 => new PR21_DD190QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_ge010 => new PR21_GE010QueryImpl(appDbContext, IdControle),
+                PR21_EnumTipoOrigem.csicp_ff140 => new PR21_FF140QueryImpl(appDbContext, IdControle),
                 _ => throw new NotImplementedException()
             };
         }
