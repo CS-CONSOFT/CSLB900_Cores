f619a1f1e1429e98d6a872003e8a0d101b4c5c84
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-10-13 09:35:51 -0300

    Update PR_32_SoliticacaoRequisicaoDespesaRepositoryTest.cs

--- CSCore.Tests.Financeiro/Repository/RequisicaoDespesa/PR_32_SoliticacaoRequisicaoDespesa/PR_32_SoliticacaoRequisicaoDespesaRepositoryTest.cs
+++ CSCore.Tests.Financeiro/Repository/RequisicaoDespesa/PR_32_SoliticacaoRequisicaoDespesa/PR_32_SoliticacaoRequisicaoDespesaRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using CSCore.Domain.Interfaces.FF.PR_32_SoliticacaoRequisicaoDespesa;
 using CSCore.Ifs.CS_Context;
 using CSCore.Ifs.FF.Repository.RequisicaoDespesa.PR_32_SoliticacaoRequisicaoDespesa;
+using Microsoft.EntityFrameworkCore;
 using NSubstitute;
 using System;
 using System.Collections.Generic;
@@ -12,19 +13,33 @@ namespace CSCore.Tests.Financeiro.Repository.RequisicaoDespesa.PR_32_Soliticacao
 {
     public class PR_32_SoliticacaoRequisicaoDespesaRepositoryTest
     {
-        private readonly AppDbContext _dbMock;
+        private readonly AppDbContext _context;
         private readonly IPR_32_SoliticacaoRequisicaoDespesaRepository _repository;
 
         public PR_32_SoliticacaoRequisicaoDespesaRepositoryTest()
         {
-            _dbMock = Substitute.For<AppDbContext>();
-            _repository = new PR_32_SoliticacaoRequisicaoDespesaRepositoryImpl(_dbMock);
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: new Guid().ToString())
+                .Options;
+
+            
+
+            _context = new AppDbContext(options);
+            _repository = new PR_32_SoliticacaoRequisicaoDespesaRepositoryImpl(_context);
         }
 
         [Fact]
         public async Task Testa_Se_FF140_Esta_Com_Status_Solicitado_E_Lanca_ErroSeTiver()
         {
+            // Arrange
+            var ff140status = new csicp_ff140
+
+
+
+            // Act
             await _repository.SolicitarRD(135);
+            // Assert
+
         }
     }
 }
