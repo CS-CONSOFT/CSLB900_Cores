089a5d2c0240ef96e0e07b45a4f8de9cc07946b9
JardelB <jardelbarros@cs-consoft.com.br>
2025-10-14 12:51:17 -0300

    feat: Adiciona suporte ao módulo RR001 e melhorias gerais
    - Adicionados novos namespaces e dependências para o domínio RR001.
    - Criada a interface IRR001Service e sua implementação RR001ServiceImpl.
    - Atualizado o repositório RR001RepositoryImpl com filtros e paginação.
    - Introduzidos filtros específicos para consultas no repositório RR001.
    - Atualizado o subprojeto CSLB900_Cores para um novo commit.
    - Melhorias na estrutura de consultas com uso de AsSplitQuery.
    - Removido código obsoleto, incluindo IStaticaLabelRepository.
    - Adicionados DTOs e manipulação de exceções em RR001ServiceImpl.
    - Melhorada a modularização e organização do código.

--- CSCore.Ifs.Rebanho/RR001Repository_CadastroAnimal/RR001RepositoryImpl.cs
+++ CSCore.Ifs.Rebanho/RR001Repository_CadastroAnimal/RR001RepositoryImpl.cs
@@ -1,8 +1,11 @@
 ﻿using CSCore.Domain.CS_Models.CSICP_RR;
 using CSCore.Domain.Interfaces.RR._00X;
 using CSCore.Domain.Interfaces.RR._00X.IRR001;
+using CSCore.Domain.Interfaces.V2;
 using CSCore.Ifs.CS_Context;
+using CSCore.Ifs.Rebanho.RR001Repository_CadastroAnimal.Filtros;
 using CSCore.Ifs.Repository;
+using CSLB900.MSTools.Extensao;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -22,28 +25,61 @@ namespace CSCore.Ifs.Rebanho.RR001Repository_CadastroAnimal
 
         public async Task<OsusrTo3CsicpRr001?> GetByIdAsync(int In_TenantID, string In_IDRR001)
         {
-            return await _appDbContext.OsusrTo3CsicpRr001s
-                .FirstOrDefaultAsync(e => e.TenantId == In_TenantID && e.Id == In_IDRR001);
+            IQueryable<OsusrTo3CsicpRr001> query = GetQueryBase(In_TenantID);
+
+            OsusrTo3CsicpRr001? CSICP_RR001 = await query
+                .FirstOrDefaultAsync(e => e.Id == In_IDRR001);
+            return CSICP_RR001;
         }
 
         public async Task<(List<OsusrTo3CsicpRr001>, int)> GetListAsync(int In_TenantID, PrmFiltrosRR001 prm)
         {
             IQueryable<OsusrTo3CsicpRr001> query = GetQueryBase(In_TenantID);
 
-            var totalCount = await query.CountAsync();
-            var items = await query.ToListAsync();
+            // Aplica filtros
+            query = AplicaFiltro(query, GetFiltrosParaAplicar(In_TenantID, prm));
 
-            return (items, totalCount);
+            var queryCount = query;
+            var count = await queryCount.CountAsync();
+
+            query = query.PaginacaoNoBanco(prm.PageNumber, prm.PageSize);
+            var listItems = await query.ToListAsync();
+
+            return (listItems, count);
         }
 
         private IQueryable<OsusrTo3CsicpRr001> GetQueryBase(int In_TenantID)
         {
             return _appDbContext.OsusrTo3CsicpRr001s
                 .AsNoTracking()
+                .AsSplitQuery()
                 .Where(e => e.TenantId == In_TenantID)
                 .Include(e => e.NavRR001Ativo)
                 .Include(e => e.NavRR001Cat)
-                .Include(e => e.NavRR001Categoria);
+                .Include(e => e.NavRR001Categoria)
+                .Include(e => e.NavRR001Fazenda)
+                .Include(e => e.NavRR001Mae)
+                .Include(e => e.NavRR001Ocorrencia)
+                .Include(e => e.NavRR001Pai)
+                .Include(e => e.NavRR001Proprietario)
+                .Include(e => e.NavRR001Raca)
+                .Include(e => e.NavRR001Sexo)
+                .Include(e => e.NavRR001Situacao);
+        }
+
+        protected override ICSFilter<OsusrTo3CsicpRr001>[] GetOutrosFiltros<TFiltros>(int TenantId, TFiltros Filtros)
+        {
+            var filtros = Filtros as PrmFiltrosRR001;
+            if (filtros == null)
+                throw new ArgumentNullException(nameof(Filtros), "Parâmetros de filtro inválidos.");
+
+            return [
+                new FiltroNomeAnimalRR001(filtros.In_Nomeanimal),
+                new FiltroNumeroRGNRR001(filtros.In_NumeroRGN),
+                new FiltroApelidoRR001(filtros.In_Apelido),
+                new FiltroAtivoIDRR001(filtros.In_AtivoID),
+                new FiltroDataNascimentoRR001(filtros.In_DataNascimento),
+            ];
         }
     }
 }
