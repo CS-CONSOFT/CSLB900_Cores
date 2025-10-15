a279127449f327371cffcf948363267c9ca4132d
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-10-15 18:02:06 -0300

    Create IFF143RepositoryImpl.cs

---
+++ CSCore.Ifs.FF/Repository/FF1XX/IFF143RepositoryImpl.cs
@@ -0,0 +1,23 @@
+﻿using CSCore.Domain.CS_Models.CSICP_FF;
+using CSCore.Domain.Interfaces.FF._1XX;
+using CSCore.Domain.Interfaces.V2;
+using CSCore.Ifs.CS_Context;
+using CSCore.Ifs.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.Repository.FF1XX
+{
+    public interface IFF143Repository : IRepositorioBase<CSICP_FF143>
+    {
+    }
+    public class IFF143RepositoryImpl : RepositorioBaseImpl<CSICP_FF143>, IFF143Repository
+    {
+        public IFF143RepositoryImpl(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
