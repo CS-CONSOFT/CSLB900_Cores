7869380fb85289b228653077ac991f6c4356b84d
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-10-17 11:38:20 -0300

    Adiciona cálculo de parcelas e testes abrangentes
    Foi criada a classe `CS_Get_Qtd_Parcelas` para calcular a quantidade de parcelas e entrada com base em condições de pagamento.
    Adicionada a classe `MemoriaCalculoGenerator` para gerar memória de cálculo detalhada, com suporte a diferentes tipos de pagamento (`Dias`, `Meses`, `À Vista`).
    Incluídas validações para cenários inválidos, como número de parcelas menor que zero e valores financeiros nulos.
    
    Foram adicionados testes unitários e de integração:
    - Testes para `CS_Get_Qtd_Parcelas` cobrindo diferentes tipos de pagamento.
    - Testes para `MemoriaCalculoGenerator`, validando cenários de borda, condições reais e distribuição de valores com restos.
    - Testes para datas limites, como anos bissextos e ajustes de fim de mês.
    
    Essas mudanças aumentam a robustez e confiabilidade do sistema.

---
+++ CSCompartilhado.CSLB900_MSTools/Calculos/MemoriaDeCalculoV2/CS_Get_Qtd_Parcelas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSLB900.MSTools.Calculos.MemoriaDeCalculoV2
+{
+    public record CS_Get_Qtd_ParcelasParameters(
+        int InStID_BB008_TP_DIAS,
+        int InStID_BB008_ParcelaDias,
+        int InStID_BB008_ParcelaMes,
+        int InStID_BB008_AVista
+        );
+
+    public record CS_Get_Qtd_ParcelasResult(
+        int Qtd_Parcelas,
+        int Entrada);
+    public static class CS_Get_Qtd_Parcelas
+    {
+        public static CS_Get_Qtd_ParcelasResult Executar(
+            string condicaoPagamento,
+            int BB008_Tipo_ID,
+            CS_Get_Qtd_ParcelasParameters prm)
+        {
+            var condicaoPagtoDividida = condicaoPagamento.Split(';');
+
+            if(BB008_Tipo_ID == prm.InStID_BB008_TP_DIAS)
+            {
+                return new CS_Get_Qtd_ParcelasResult(
+                    Qtd_Parcelas: condicaoPagtoDividida.Length,
+                    Entrada: 0);
+            }
+
+            if(BB008_Tipo_ID == prm.InStID_BB008_ParcelaMes || BB008_Tipo_ID == prm.InStID_BB008_ParcelaDias)
+            {
+                return new CS_Get_Qtd_ParcelasResult(
+                    Qtd_Parcelas: int.Parse(condicaoPagtoDividida[0]),
+                    Entrada: int.Parse(condicaoPagtoDividida[1]));
+            }
+                
+
+            return new CS_Get_Qtd_ParcelasResult(
+                Qtd_Parcelas: 1,
+                Entrada: 0);
+        }
+    }
+}
