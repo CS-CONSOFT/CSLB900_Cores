f6239435dc6c8aab2910e9a7f0b8d032773766d6
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-10-17 14:40:07 -0300

    Adiciona suporte a filtro por tipo de espécie
    - Substituído namespace em `ConfigurarControllers.cs`.
    - Adicionado endpoint `GetListPorTipoEspecie` em `BB026Controller.cs`.
    - Atualizada interface `ICS_BB026Service` com novo método `GetList`.
    - Implementado método `GetList` em `BB026Service` para suporte a filtro.
    - Alterada herança de `FiltrosTextoIsAtivo` para nova classe base.
    - Criado enum `TIPO_ESPECIE` em `IBB026Repository`.
    - Implementado método `GetListAsyncPorTipoEspecie` em `BB026Repository`.
    - Atualizado subprojeto `CSLB900_Cores` para nova versão.
    - Removido import obsoleto em `BaseServiceImpl.cs`.
    - Adicionado construtor em `BB026Repository` para inicializar contexto.
    - Adicionado tratamento de exceções no método `GetList` de `BB026Service`.
    
    Essas mudanças expandem a funcionalidade do sistema para suportar consultas filtradas por tipo de espécie (`A Receber` ou `A Pagar`) e incluem melhorias gerais no código.

--- CSCore.Domain/CS_QueryFilters/FiltrosTextoIsAtivo.cs
+++ CSCore.Domain/CS_QueryFilters/FiltrosTextoIsAtivo.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSLB900.MSTools.CS_QueryFilters
 {
-    public class FiltrosTextoIsAtivo : ParametrosBaseFiltro
+    public class FiltrosTextoIsAtivo : ParametrosBaseFiltroSemExcederOMaxPageSize
     {
         public string? Search { get; set; } = null;
         public bool IsActive { get; set; } = true;
--- CSCore.Domain/Interfaces/BB/IBB026Repository.cs
+++ CSCore.Domain/Interfaces/BB/IBB026Repository.cs
@@ -1,10 +1,16 @@
 ﻿namespace CSCore.Domain.Interfaces.BB
 {
+    public enum TIPO_ESPECIE
+    {
+        AReceber = 0,
+        APagar = 1
+    }
     public interface IBB026Repository : IBaseCrud<CSICP_Bb026>
     {
         Task<CSICP_Bb026?> GetByIdAsync(string id, int tenant);
         Task<IEnumerable<CSICP_Bb026>> GetListAsync(int tenant, string? search, int? searchCode, bool? isActive);
         Task<CSICP_Bb026> ChangeActive(CSICP_Bb026 entity);
+        Task<IEnumerable<CSICP_Bb026>> GetListAsyncPorTipoEspecie(int tenant, string? search, int? searchCode, bool? isActive, TIPO_ESPECIE tIPO_ESPECIE);
     }
 }
 
