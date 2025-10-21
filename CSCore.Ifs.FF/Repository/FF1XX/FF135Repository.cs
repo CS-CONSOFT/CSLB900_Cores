4935a49064db6950b27c145f03b5881f28f5992d
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-10-21 17:28:57 -0300

    Adiciona serviço e repositório para baixa de cartas
    Implementa a interface e classe `PR17_CartadeDebitoProcessaBaixaService` com o método `Executar`, responsável por validar e processar baixas de cartas de débito.
    
    Adiciona a interface `IFF135Repository` e sua implementação, permitindo a busca de registros na tabela `OsusrE9aCsicpFf135s` com base no `TenantId` e no `Ff135CartadebitoId`.
    
    Inclui validações de saldo e criação de registros do tipo `CSICP_FF103` no processo de baixa.

---
+++ CSCore.Ifs.FF/Repository/FF1XX/FF135Repository.cs
@@ -0,0 +1,36 @@
+﻿using CSCore.Domain.CS_Models.CSICP_FF;
+using CSCore.Ifs.CS_Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.FF.Repository.FF1XX
+{
+    public interface IFF135Repository
+    {
+        Task<CSICP_FF135> GetById(int tenant, string id);
+    }
+    public class FF135Repository : IFF135Repository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public FF135Repository(AppDbContext appDbContext)
+        {
+            this.appDbContext = appDbContext;
+        }
+
+        public async Task<CSICP_FF135> GetById(int tenant, string id)
+        {
+            var WorkFF135 = await this.appDbContext.OsusrE9aCsicpFf135s
+                .Where(e => e.TenantId == tenant)
+                .Where(e => e.Ff135CartadebitoId == id)
+                .FirstOrDefaultAsync() ?? throw new Exception("Registro não encontrado");
+
+
+            return WorkFF135;
+        }
+    }
+}
