05ab95daae2d0e10ca859a767f39aa979f9a7339
JardelB <jardelbarros@cs-consoft.com.br>
2025-10-24 11:57:57 -0300

    feat: Adiciona funcionalidade de processamento de lotes
    - Adicionada referência ao projeto `CSCore.Ifs.Rebanho` no `.csproj`.
    - Criados serviços e repositórios para processar lotes e atualizar pesos.
    - Adicionado controlador `ProcessoLoteController` com endpoint para API.
    - Atualizado `IUnitOfWorkV2` com novas propriedades para o processo.
    - Refatorados métodos de cálculo de datas em `RR030ServiceImpl`.
    - Adicionadas validações de intervalo no DTO `DtoCreateUpdateRR030`.
    - Atualizados mapeamentos em `RR002Mapper` e `RR021Mapper`.
    - Criado DTO `DtoGetRR021_ComRR001eRR022` para navegações.
    - Configurada injeção de dependência para novos serviços e repositórios.
    - Atualizado subprojeto `CSLB900_Cores` para novo commit.

---
+++ CSCore.Ifs.Rebanho/RR021Repository_LoteVsAnimal/ProcessaLote/IProcessaLoteEAtualizaPeso.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Ifs.Rebanho.RR021Repository_LoteVsAnimal.ProcessaLote
+{
+    public interface IProcessaLoteEAtualizaPeso
+    {
+        Task GetExecutaProcessoLote(int InTenantID, string InRR020IDLote);
+    }
+}
