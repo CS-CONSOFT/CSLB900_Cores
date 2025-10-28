d90c4eb70b76734b8a59dd0e7ab368756c42efac
valtecsgabriel <valter.gabriel@cs-consoft.com.br>
2025-10-28 17:16:50 -0300

    Adiciona suporte à paginação em FF135
    Refatora o método `GetListCartaDeDebito` para retornar um objeto `PagedList<CSICP_FF135>` com suporte a paginação, adicionando os parâmetros `page` e `pageSize` na interface e implementação de `IFF135ServiceImpl` e `IFF135Repository`.
    
    Cria o controlador `FF135Controller` com o endpoint `GET /ff105/v1/ff135/lista-carta-debito` para expor a funcionalidade de consulta paginada.
    
    Atualiza o repositório para realizar paginação diretamente no banco de dados, utilizando a extensão `PaginacaoNoBanco` e retornando uma tupla com a lista de itens e o total de registros.
    
    Adiciona novos namespaces e dependências para suportar as alterações. Ajusta o código para melhorar a experiência do usuário ao lidar com grandes volumes de dados.

--- CSCore.Ifs.FF/Repository/FF1XX/FF135Repository.cs
+++ CSCore.Ifs.FF/Repository/FF1XX/FF135Repository.cs
@@ -1,5 +1,6 @@
 ﻿using CSCore.Domain.CS_Models.CSICP_FF;
 using CSCore.Ifs.CS_Context;
+using CSLB900.MSTools.Extensao;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,7 +13,7 @@ namespace CSCore.Ifs.FF.Repository.FF1XX
     public interface IFF135Repository
     {
         Task<CSICP_FF135> GetById(int tenant, string id);
-        Task<List<CSICP_FF135>> GetListCartaDeDebito(int tenant, DateTime DataInicial, DateTime DataFinal, string BB012_ContaID, int status);
+        Task<(List<CSICP_FF135>, int)> GetListCartaDeDebito(int tenant, DateTime DataInicial, DateTime DataFinal, string BB012_ContaID, int status, int page, int pageSize);
     }
     public class FF135Repository : IFF135Repository
     {
@@ -34,19 +35,22 @@ namespace CSCore.Ifs.FF.Repository.FF1XX
             return WorkFF135;
         }
 
-        public async Task<List<CSICP_FF135>> GetListCartaDeDebito(int tenant, DateTime DataInicial, DateTime DataFinal, string BB012_ContaID, int status)
+        public async Task<(List<CSICP_FF135>, int)> GetListCartaDeDebito(int tenant, DateTime DataInicial, DateTime DataFinal, string BB012_ContaID, int status, int page, int pageSize)
         {
 
-            var WorkFF135 = await this.appDbContext.OsusrE9aCsicpFf135s
+            var query = this.appDbContext.OsusrE9aCsicpFf135s
                 .Where(e => e.TenantId == tenant)
                 .Where(e => e.Ff135DataMovto >= DataInicial)
                 .Where(e => e.Ff135DataMovto <= DataFinal)
                 .Where(e => e.Ff135Statusid <= status)
-                .Where(e => e.Ff135ContafornecId == BB012_ContaID)
-                .ToListAsync() ?? throw new Exception("Registro não encontrado");
+                .Where(e => e.Ff135ContafornecId == BB012_ContaID);
 
+            var queryCount = query;
+            var count = queryCount.Count();
+            query = query.OrderBy(e => e.Ff135CartadebitoId);
+            query = query.PaginacaoNoBanco(page, pageSize);
 
-            return WorkFF135;
+            return (await query.ToListAsync(), count);
         }
     }
 }
