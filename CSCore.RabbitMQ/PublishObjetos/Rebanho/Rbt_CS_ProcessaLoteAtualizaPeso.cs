35c09fac08e488ec16dcfbcd6a24e1666ed54f24
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-07 15:45:21 -0300

    feat: Integração com RabbitMQ para processamento de lotes
    - Adicionada dependência ao projeto `CSCore.RabbitMQ` nos arquivos `.csproj` e na solução `.sln`.
    - Configurado o consumidor `EvtBusProcessaLoteAtualizaPeso` no RabbitMQ para processar mensagens da fila `Queue_ProcessaLoteEAtualizaPesoRebanho`.
    - Alterado o `ProcessoLoteController` para enviar mensagens ao RabbitMQ em vez de processar diretamente.
    - Criado o consumidor `EvtBusProcessaLoteAtualizaPeso` com integração ao SignalR para notificação de clientes.
    - Adicionadas novas chaves de roteamento no `RoutingKeys.cs` para o processamento de lotes.
    - Atualizada a classe `Rbt_CS_ProcessaLoteAtualizaPeso` para implementar `IConsumerUsuarioId` e evitar serialização de `TenantID`.
    - Atualizado o submódulo `CSLB900_Cores` para um novo commit.
    - Ajustes gerais de nomenclatura e remoção de código obsoleto.

--- CSCore.RabbitMQ/PublishObjetos/Rebanho/Rbt_CS_ProcessaLoteAtualizaPeso.cs
+++ CSCore.RabbitMQ/PublishObjetos/Rebanho/Rbt_CS_ProcessaLoteAtualizaPeso.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using CSCore.RabbitMQ.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CSCore.RabbitMQ.PublishObjetos.Rebanho
 {
-    public class Rbt_CS_ProcessaLoteAtualizaPeso
+    public class Rbt_CS_ProcessaLoteAtualizaPeso : IConsumerUsuarioId
     {
         /// <summary>
         /// ID do Tenant
         /// </summary>
+        [JsonIgnore]
         public int TenantID { get; set; }
 
         /// <summary>
