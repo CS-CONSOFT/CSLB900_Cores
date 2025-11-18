f21625812fbb5ae9dcbe4eb2dbe48e0584a0f90f
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-18 18:23:36 -0300

    feat: Adiciona suporte para CG080 com DTOs e repositório
    Introduz novas classes DTO (`DtoCreateUpdateCG080` e `DtoGetCG080`) para facilitar a transferência de dados da entidade `Osusr8dwCsicpCg080`. Adiciona o mapeador `CG080Mapper` para conversão entre entidade e DTOs. Cria a interface `ICG080Repository` e sua implementação `CG080RepositoryImpl` para operações de banco de dados, incluindo métodos assíncronos para busca por ID e listagem com paginação e filtragem. Atualiza o subprojeto `CSLB900_Cores` para um novo commit.

---
+++ CSCore.Domain/Interfaces/CG/CG08X/CG080/ICG080Repository.cs
@@ -0,0 +1,12 @@
+using CSCore.Domain.CS_Models.CSICP_CG;
+using CSCore.Domain.Interfaces.V2;
+using CSLB900.MSTools.InterfaceBase;
+
+namespace CSCore.Domain.Interfaces.CG.CG08X.CG080
+{
+    public interface ICG080Repository : IRepositorioBaseV2<Osusr8dwCsicpCg080>
+    {
+        Task<Osusr8dwCsicpCg080?> GetByIdAsync(int InTenantID, int InCG080ID);
+        Task<(List<Osusr8dwCsicpCg080>, int)> GetListAsync(int InTenantID, string? InNome, int InPageNumber, int InPageSize);
+    }
+}
