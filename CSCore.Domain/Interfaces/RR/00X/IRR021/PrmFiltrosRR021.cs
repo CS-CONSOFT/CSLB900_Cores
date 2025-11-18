a779fe8537afc8e346c07f720e2b07ad2a5f4be7
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-18 09:26:46 -0300

    refactor: Tornar parâmetros obrigatórios e atualizar heranças
    Adicionados atributos [Required] aos parâmetros InTenantID e InUsuarioID nos métodos PopulaPesoAnimal e GetById em RR022Controller.cs, tornando-os obrigatórios nas requisições HTTP. Atualizado o commit do subprojeto CSLB900_Cores. Alteradas as classes PrmFiltrosRR021 e PrmFiltrosRR022 para herdar de ParametrosBaseFiltroSemExcederOMaxPageSize, possivelmente afetando a lógica de paginação.

--- CSCore.Domain/Interfaces/RR/00X/IRR021/PrmFiltrosRR021.cs
+++ CSCore.Domain/Interfaces/RR/00X/IRR021/PrmFiltrosRR021.cs
@@ -3,7 +3,7 @@ using CSLB900.MSTools.CS_QueryFilters;
 
 namespace CSCore.Domain.Interfaces.RR._00X.IRR021
 {
-    public class PrmFiltrosRR021 : ParametrosBaseFiltro
+    public class PrmFiltrosRR021 : ParametrosBaseFiltroSemExcederOMaxPageSize
     {
         public string? In_AnimalId { get; set; }
         public DateTime? In_DtRegistroInicio { get; set; }
--- CSCore.Domain/Interfaces/RR/00X/IRR022/PrmFiltrosRR022.cs
+++ CSCore.Domain/Interfaces/RR/00X/IRR022/PrmFiltrosRR022.cs
@@ -3,7 +3,7 @@ using CSLB900.MSTools.CS_QueryFilters;
 
 namespace CSCore.Domain.Interfaces.RR._00X.IRR022
 {
-    public class PrmFiltrosRR022 : ParametrosBaseFiltro
+    public class PrmFiltrosRR022 : ParametrosBaseFiltroSemExcederOMaxPageSize
     {
         public string? In_LoteId { get; set; }
         public DateTime? In_DataPeso { get; set; }
