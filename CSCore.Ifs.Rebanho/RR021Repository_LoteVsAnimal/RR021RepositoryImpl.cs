174ff0b0466c963fe9b48caafb6fb029fa6087c5
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-19 11:38:52 -0300

    refactor: Refatora GetListRR021ParaPopular e ajusta RR022ServiceImpl
    Modifica a função GetListRR021ParaPopular para retornar apenas
    uma lista de OsusrTo3CsicpRr021, removendo a contagem total.
    Ajusta a chamada em RR022ServiceImpl para refletir essa mudança,
    removendo parâmetros desnecessários e a variável totalCount.
    
    Simplifica a consulta LINQ em RR021RepositoryImpl, removendo
    junções e condições relacionadas a Rr022Usuarioid e Rr022Dtpeso.
    
    Atualiza o commit do subprojeto em CSLB900_Cores.
    
    Remove o bloco de verificação de existência de registro em
    RR022ServiceImpl, alterando a lógica de fluxo.

--- CSCore.Ifs.Rebanho/RR021Repository_LoteVsAnimal/RR021RepositoryImpl.cs
+++ CSCore.Ifs.Rebanho/RR021Repository_LoteVsAnimal/RR021RepositoryImpl.cs
@@ -53,22 +53,17 @@ namespace CSCore.Ifs.Rebanho.RR021Repository_LoteVsAnimal
             ];
         }
 
-        public async Task<(List<OsusrTo3CsicpRr021>, int)> GetListRR021ParaPopular(int In_TenantID, string In_UsuarioID, string In_LoteID, DateTime In_DataPeso)
+        public async Task<List<OsusrTo3CsicpRr021>> GetListRR021ParaPopular(int In_TenantID, string In_LoteID)
         {
             var query = from rr021 in _appDbContext.OsusrTo3CsicpRr021s
                         .AsNoTracking()
 
-                        join rr022 in _appDbContext.OsusrTo3CsicpRr022s
-                        on rr021.Id equals rr022.Rr022Loteid into rr022Join_rr021
-                        from rr022 in rr022Join_rr021.DefaultIfEmpty()
                         where rr021.TenantId == In_TenantID
                             && rr021.Rr021Loteid == In_LoteID
-                            && rr022.Rr022Usuarioid == In_UsuarioID
-                            && rr022.Rr022Dtpeso == In_DataPeso
-                            select rr021;
+                        select rr021; 
 
             var listRR021Items = await query.ToListAsync();
-            return (listRR021Items, listRR021Items.Count);
+            return listRR021Items;
         }
 
     }
