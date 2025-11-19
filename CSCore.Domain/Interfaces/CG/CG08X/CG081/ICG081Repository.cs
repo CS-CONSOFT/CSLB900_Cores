fe53a823dcb2c9745b51d013692359ceefe0b0bb
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-19 18:05:39 -0300

    feat: Adiciona suporte para manipulação de CG081
    Foram adicionadas novas classes DTO (`DtoCreateUpdateCG081` e `DtoGetCG081`) para facilitar a criação, atualização e obtenção de dados da entidade `Osusr8dwCsicpCg081`. Um mapeador estático `CG081Mapper` foi implementado para conversão entre entidade e DTO.
    
    Além disso, foi criada a interface `ICG081Repository` e sua implementação `CG081RepositoryImpl` para operações de banco de dados, incluindo métodos para obter uma entidade por ID e listar entidades com paginação.
    
    Atualização do subprojeto `CSLB900_Cores` para o commit `0d5c329dda6c5a3927692b43770c1add9a780baa`.

---
+++ CSCore.Domain/Interfaces/CG/CG08X/CG081/ICG081Repository.cs
@@ -0,0 +1,12 @@
+using CSCore.Domain.CS_Models.CSICP_CG;
+using CSCore.Domain.Interfaces.V2;
+using CSLB900.MSTools.InterfaceBase;
+
+namespace CSCore.Domain.Interfaces.CG.CG08X.CG081
+{
+    public interface ICG081Repository : IRepositorioBase<Osusr8dwCsicpCg081>
+    {
+        Task<Osusr8dwCsicpCg081?> GetByIdAsync(int InTenantID, long InCG081ID);
+        Task<(List<Osusr8dwCsicpCg081>, int)> GetListAsync(int InTenantID, long InCG081ID, string InUsuarioID, int InPageNumber, int InPageSize);
+    }
+}
