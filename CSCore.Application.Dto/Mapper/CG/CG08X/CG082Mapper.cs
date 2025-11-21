ed6fdd5201770a8c360af629ae56a3bbd40da44d
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-21 11:48:29 -0300

    feat: Adiciona suporte a CG080 e CG081
    - Adicionadas dependências para CG080 e CG081 em vários arquivos.
    - Atualizado `ConfigurationRepositories.cs` e `ConfigurationServices.cs` para incluir repositórios e serviços CG080 e CG081.
    - Modificada `IUnitOfWorkV2` e `UnitOfWorkV2Impl` para suportar `ICG081Repository`.
    - Criada interface `ICG081Service` e implementada `CG081ServiceImpl`.
    - Criado controlador `CG081Controller` com endpoints CRUD.
    - Corrigido método `GetListAsync` em `ICG081Repository` e `CG081RepositoryImpl`.
    - Adicionados arquivos para suporte a CG082: `DtoGetCG082.cs`, `CG082Mapper.cs`, `ICG082Repository.cs`, e `CG082RepositoryImpl.cs`.

---
+++ CSCore.Application.Dto/Mapper/CG/CG08X/CG082Mapper.cs
@@ -0,0 +1,21 @@
+using CSCore.Application.Dto.Dtos.CG.CG082;
+using CSCore.Domain.CS_Models.CSICP_CG;
+
+namespace CSCore.Application.Dto.Mapper.CG.CG08X
+{
+    public static class CG082Mapper
+    {
+        public static DtoGetCG082 ToDtoGet(this Osusr8dwCsicpCg082 entity)
+        {
+            return new DtoGetCG082
+            {
+                TenantId = entity.TenantId,
+                Cg082Id = entity.Cg082Id,
+                Cg082Contrelregid = entity.Cg082Contrelregid,
+                Cg082Contcontaid = entity.Cg082Contcontaid,
+                Cg082Dateinicial = entity.Cg082Dateinicial,
+                Cg082Datefinal = entity.Cg082Datefinal
+            };
+        }
+    }
+}
