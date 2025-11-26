9cb832ab216c28e93fcc09d5054541cf8d8ba732
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-26 08:29:20 -0300

    refactor: Simplifica Processamento de Peso Animal
    Remove dependência de IProcessaRegistroPesoAnimal e método síncrono em ProcessaPesoAnimalController. Renomeia método assíncrono para ProcessarPesoAnimal e adiciona verificação obrigatória para Usuario_ID. Marca commit de CSLB900_Cores como "dirty". Adiciona anotação [Required] para In_DataPeso em PrmProcessaPesoRR022.

--- CSCore.Domain/Interfaces/RR/00X/IRR022/PrmProcessaPesoRR022.cs
+++ CSCore.Domain/Interfaces/RR/00X/IRR022/PrmProcessaPesoRR022.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,8 @@ namespace CSCore.Domain.Interfaces.RR._00X.IRR022
     public class PrmProcessaPesoRR022
     {
         public string? In_LoteId { get; set; }
+
+        [Required(ErrorMessage = "A data do peso é obrigatória para processamento.")]
         public DateTime? In_DataPeso { get; set; }
     }
 }
