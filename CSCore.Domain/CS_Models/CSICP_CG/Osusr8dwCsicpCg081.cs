50061303cc8cbc7d391b43d9ca35cbcb97292a69
JardelB <jardelbarros@cs-consoft.com.br>
2025-11-27 18:09:56 -0300

    feat: Adiciona novas interfaces e implementações de repositório
    Atualiza o subprojeto `CSLB900_Cores` para um novo commit.
    Adiciona propriedades com chaves estrangeiras e de navegação
    nos arquivos `Osusr8dwCsicpCg081.cs` e `Osusr8dwCsicpCg082.cs`.
    Cria interfaces `ICG060Repository` e `ICG063Repository` com
    métodos assíncronos para manipulação de dados.
    Implementa classes `CG060RepositoryImpl` e `CG063RepositoryImpl`
    para fornecer lógica aos métodos definidos nas interfaces.

--- CSCore.Domain/CS_Models/CSICP_CG/Osusr8dwCsicpCg081.cs
+++ CSCore.Domain/CS_Models/CSICP_CG/Osusr8dwCsicpCg081.cs
@@ -1,6 +1,8 @@
-﻿using CSCore.Domain.CS_Models.Staticas.CG;
+﻿using CSCore.Domain.CS_Models.CSICP_GG;
+using CSCore.Domain.CS_Models.Staticas.CG;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CSCore.Domain.CS_Models.CSICP_CG;
 
@@ -10,14 +12,18 @@ public partial class Osusr8dwCsicpCg081
 
     public long Cg081Id { get; set; }
 
+    [ForeignKey("NavCG081ContRelConf")]
     public long? Cg081Contrelconfid { get; set; }
 
+    [ForeignKey("NavCG081ASID")]
     public int? Cg081Asid { get; set; }
 
     public string? Cg081Txdescricao { get; set; }
 
+    [ForeignKey("NavCG081ContRelRegistroSup")]
     public long? Cg081Contrelregistrosup { get; set; }
 
+    [ForeignKey("NavNaturezaSaldo")]
     public int? Cg081Naturezasaldo { get; set; }
 
     public long? Cg081Nrlinha { get; set; }
@@ -37,4 +43,9 @@ public partial class Osusr8dwCsicpCg081
     public bool? Cg081Isnewpage { get; set; }
 
     public int? Cg081Treeorder { get; set; }
+
+    public Osusr8dwCsicpCg080? NavCG081ContRelConf { get; set; }
+    public Osusr8dwCsicpCg997? NavCG081ASID { get; set; }
+    public Osusr8dwCsicpCg081? NavCG081ContRelRegistroSup { get; set; }
+    public OsusrE9aCsicpGg993? NavCG993NaturezaSaldo { get; set; }
 }
