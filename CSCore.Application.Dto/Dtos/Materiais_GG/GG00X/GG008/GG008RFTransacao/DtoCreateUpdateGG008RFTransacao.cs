f6a0bad5c36d3bfb06cfe9338cf89cbdd1b542d2
JardelB <jardelbarros@cs-consoft.com.br>
2025-12-11 16:15:31 -0300

    feat: Adiciona suporte para GG008RFTransacao
    - Adicionado o serviço `IGG008RFTransacaoService` e sua implementação `GG008RFTransacaoServiceImpl` para gerenciar operações da entidade `GG008RFTransacao`.
    - Criado o repositório `IGG008RFTransacaoRepository` e sua implementação `GG008RFTransacaoRepositoryImpl` para lidar com operações de banco de dados.
    - Atualizado `ConfigurationServices.cs` para registrar o novo serviço e repositório no container de injeção de dependência.
    - Modificada a interface `IUnityOfWorkGG` e a classe `UowGG` para incluir suporte ao novo serviço e repositório.
    - Criado o controlador `GG008RFTransacaoController` com endpoints REST para operações CRUD.
    - Desenvolvidos os DTOs `DtoCreateUpdateGG008RFTransacao` e `DtoGetGG008RFTransacao`, além do mapeador `GG008RFTransacaoMapper`.
    - Atualizado o subprojeto `CSLB900_Cores` para o commit `6ef814550caf37a66cce8654a88fce5e5e804a0a`.
    - Implementados os métodos `GetByIdAsync` e `GetListAsync` no repositório para suporte a filtros e paginação.
    - Adicionadas anotações Swagger para documentação dos endpoints.
    - Integrado o uso de ferramentas existentes, como `CSLB900.MSTools`, para geração de IDs, paginação e tratamento de exceções.

---
+++ CSCore.Application.Dto/Dtos/Materiais_GG/GG00X/GG008/GG008RFTransacao/DtoCreateUpdateGG008RFTransacao.cs
@@ -0,0 +1,37 @@
+﻿using CSCore.Domain.DELETAR;
+using CSLB900.MSTools.InterfaceBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Application.Dto.Dtos.Materiais_GG.GG00X.GG008.GG008RFTransacao
+{
+    public class DtoCreateUpdateGG008RFTransacao : IConverteParaEntidade<OsusrE9aCsicpGg008rftransacao>
+    {
+        public int? Gg008tTiporegistro { get; set; }
+
+        public string? Gg008tFilialid { get; set; }
+
+        public string? Gg008tKardexId { get; set; }
+
+        public string? Gg008tNcmId { get; set; }
+
+        public string? Gg008tTransacaoid { get; set; }
+
+        public OsusrE9aCsicpGg008rftransacao ToEntity(int tenant, string? id)
+        {
+            return new OsusrE9aCsicpGg008rftransacao
+            {
+                TenantId = tenant,
+                Id = id!,
+                Gg008tTiporegistro = this.Gg008tTiporegistro,
+                Gg008tFilialid = this.Gg008tFilialid,
+                Gg008tKardexId = this.Gg008tKardexId,
+                Gg008tNcmId = this.Gg008tNcmId,
+                Gg008tTransacaoid = this.Gg008tTransacaoid
+            };
+        }
+    }
+}
---
+++ CSCore.Application.Dto/Dtos/Materiais_GG/GG00X/GG008/GG008RFTransacao/DtoGetGG008RFTransacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCore.Application.Dto.Dtos.Materiais_GG.GG00X.GG008.GG008RFTransacao
+{
+    public class DtoGetGG008RFTransacao
+    {
+        public int TenantId { get; set; }
+
+        public string Id { get; set; } = null!;
+
+        public int? Gg008tTiporegistro { get; set; }
+
+        public string? Gg008tFilialid { get; set; }
+
+        public string? Gg008tKardexId { get; set; }
+
+        public string? Gg008tNcmId { get; set; }
+
+        public string? Gg008tTransacaoid { get; set; }
+    }
+}
