c2cdb89dd1e5c98f8c209403693e2edb663adcdb
ValterGabriell <valter.gabriel@cs-consoft.com.br>
2025-12-15 12:09:37 -0300

    Refatora integração RabbitMQ/MassTransit em AnaliseCredito
    - Atualiza referências de submódulos e dependências no .sln e .csproj
    - Adiciona MassTransit e RabbitMQ ao projeto AnaliseDeCredito
    - Remove consumer antigo e implementa BaseConsumer<T> abstrata
    - Centraliza lógica de notificação e logging via Service Center e API externa
    - Move Rbt_CS_AnaliseCredito para novo namespace
    - Remove referências e arquivos obsoletos, consolidando responsabilidades no projeto de análise de crédito

---
+++ CSCore.Ifs.AnaliseDeCredito/RabbitMQ/EvtBusAnaliseDeCredito.cs
@@ -0,0 +1,62 @@
+﻿using CSCore.Ifs.AnaliseDeCredito.AnaliseCredito;
+using CSCore.Ifs.AnaliseDeCredito.RabbitMQ;
+using CSCore.Ifs.Compartilhado;
+using CSLB900.MSTools.Util;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using Serilog;
+using System.Text.Json;
+
+namespace CSCore.RabbitMQ.Consumers
+{
+    public class EvtBusAnaliseDeCredito(CreditoSemScore creditoSemScore,
+       IRepoSaveLogServiceCenter repoSaveLogServiceCenter) : BaseConsumer<Rbt_CS_AnaliseCredito>(repoSaveLogServiceCenter)
+    {
+        private CreditoSemScore _creditoSemScore = creditoSemScore;
+
+        public async Task Consume(ConsumeContext<Rbt_CS_AnaliseCredito> context)
+        {
+            Log.Information("RabbitMQ: Mensagem recebida no consumer {Consumer} às {Data}. Tipo da mensagem: {MessageType}. Conteúdo: {@Message}",
+                    this.GetType().Name,
+                    DateTime.UtcNow.ToLocalTime(),
+                    context.Message.GetType().Name,
+                    context.Message);
+            try
+            {
+
+                await _creditoSemScore.ExecutarAnaliseCredito(
+                    context.Message.in_CalcularScoreClearsale,
+                    context.Message.in_tenantID,
+                    context.Message.in_conta_ID);
+
+
+                await NotificarSucessoProcessamento(
+                   UsuarioID: context.Message.in_usuarioID,
+                   Tenant: context.Message.in_tenantID,
+                   context: context,
+                   Message: "Sucesso ao realizar analise de credito",
+                   Group: "analise-de-credito",
+                   Method: "ANALISE_CREDITO",
+                  JsonParametrosParaServiceCenter: JsonSerializer.Serialize(new
+                  {
+                      PrmAnaliseDeCredito = context.Message
+                  }));
+            }
+            catch (Exception ex)
+            {
+                await NotificarFalhaProcessamento(
+                      UsuarioID: context.Message.in_usuarioID,
+                      Tenant: context.Message.in_tenantID,
+                      context: context,
+                   Message: "Falha ao realizar analise de credito: " + HandlerExceptionMessage.CreateExceptionMessage(ex),
+                   Group: "analise-de-credito",
+                   Method: "ANALISE_CREDITO",
+                  JsonParametrosParaServiceCenter: JsonSerializer.Serialize(new
+                  {
+                      PrmAnaliseDeCredito = context.Message
+                  }));
+                throw new Exception(HandlerExceptionMessage.CreateExceptionMessage(ex));
+            }
+        }
+    }
+}
