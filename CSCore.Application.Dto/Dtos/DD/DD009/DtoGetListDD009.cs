c1bcc9c179296fe9382b0ba6d5538b5360952ba5
Jardel Barros <jardelbarros@cs-consoft.com.br>
2026-01-20 18:52:46 -0300

    feat: Cria CRUDS de tabelas DDXXX, feito melhorias em filtros, DTOs, protocolos e repositórios
    - Controllers DD001, DD013 e DD016 agora geram protocolo automaticamente via IGenerateProtocolo.
    - Filtros dinâmicos aprimorados nos métodos GetAll, incluindo suporte ao tipo "Contém".
    - Novos DTOs simplificados para DD001; DTOs de DD009/DD016 agora retornam dados de navegação.
    - Removido campo de protocolo dos DTOs de criação/atualização de DD013.
    - Mapeamentos ajustados para entidades relacionadas.
    - Implementado repositório customizado DD009 com includes e paginação; criada interface IDD009Repository.
    - Enum TipoFiltroDinamico agora suporta "Contém"; método de aplicação de filtros ajustado.
    - Correções de nomenclatura, ForeignKey e consulta SQL; atualização do submódulo CSLB900_Cores.

--- CSCore.Application.Dto/Dtos/DD/DD009/DtoGetListDD009.cs
+++ CSCore.Application.Dto/Dtos/DD/DD009/DtoGetListDD009.cs
@@ -1,3 +1,8 @@
+using CSBS101._82Application.Dto.BB00X.BB001;
+using CSBS101._82Application.Dto.BB00X.BB026;
+using CSBS101._82Application.Dto.BB00X.BB029;
+using CSBS101._82Application.ExtensionsMethods.BB00X;
+using CSBS101._82Application.Mapper.BB00X.BB00X.BB001;
 using CSCore.Domain.CS_Models.CSICP_DD;
 using CSLB900.MSTools.InterfaceBase;
 
@@ -12,6 +17,12 @@ namespace CSCore.Application.Dto.Dtos.DD.DD009
         public string? Dd009FormapagtoId { get; init; }
         public bool? Dd009Isactive { get; init; }
 
+        public Dto_GetBB001_Exibicao? NavBB001EmpresaID_DD009 { get; init; }
+
+        public Dto_GetBB029? NavBB029CategoriaID_DD009 { get; init; }
+
+        public Dto_GetBB026_Exibicao? NavBB026FormaPagtoID_DD009 { get; init; }
+
         public static DtoGetListDD009 FromEntity(CSICP_DD009 entity)
         {
             return new DtoGetListDD009
@@ -21,7 +32,10 @@ namespace CSCore.Application.Dto.Dtos.DD.DD009
                 Dd009Empresaid = entity.Dd009Empresaid,
                 Dd009CategoriaId = entity.Dd009CategoriaId,
                 Dd009FormapagtoId = entity.Dd009FormapagtoId,
-                Dd009Isactive = entity.Dd009Isactive
+                Dd009Isactive = entity.Dd009Isactive,
+                NavBB001EmpresaID_DD009 = entity.NavBB001EmpresaID_DD009?.ToDtoGetExibicao(),
+                NavBB029CategoriaID_DD009 = entity.NavBB029CategoriaID_DD009?.ToDtoGet(),
+                NavBB026FormaPagtoID_DD009 = entity.NavBB026FormaPagtoID_DD009?.ToDtoGetExibicao(),
             };
         }
     }
--- CSCore.Application.Dto/Dtos/DD/DD016/DtoGetDD016.cs
+++ CSCore.Application.Dto/Dtos/DD/DD016/DtoGetDD016.cs
@@ -1,6 +1,20 @@
+using CSBS101._82Application.Dto.BB00X.BB001;
+using CSBS101._82Application.Mapper.BB00X;
+using CSBS101._82Application.Mapper.BB00X.BB00X.BB001;
+using CSBS101.C82Application.Dto.BB00X.BB00X.BB008;
+using CSCore.Application.Dto.Dtos.DD.DD001;
+using CSCore.Application.Dto.Dtos.Materiais_GG.GG00X.GG006;
+using CSCore.Application.Dto.Mapper.DD;
+using CSCore.Application.Dto.Mapper.GG00X;
+using CSCore.Domain;
 using CSCore.Domain.CS_Models.CSICP_DD;
+using CSCore.Domain.CS_Models.CSICP_GG;
 using CSCore.Domain.Interfaces;
 using CSLB900.MSTools.InterfaceBase;
+using FF105Financeiro.C82Application.Dto.GG00X.GG003;
+using FF105Financeiro.C82Application.Dto.GG00X.GG004;
+using FF105Financeiro.C82Application.Dto.GG00X.GG005;
+using FF105Financeiro.C82Application.Dto.GG00X.GG006;
 
 namespace CSCore.Application.Dto.Dtos.DD.DD016
 {
@@ -22,6 +36,15 @@ namespace CSCore.Application.Dto.Dtos.DD.DD016
         public string? Dd016Protocolnumber { get; init; }
         public string? Dd001Rcomercializid { get; init; }
 
+        public Dto_GetBB001_Exibicao? NavBB001FilialID_DD016 { get; init; }
+        public Dto_GetBB008_Exibicao? NavBB008CondicaoID_DD016 { get; init; }
+        public DtoGetGG003_Exibicao_2? NavGG003GrupoID_DD016 { get; init; }
+        public DtoGetGG004Simples? NavGG004ClasseID_DD016 { get; init; }
+        public DtoGetGG005Simples? NavGG005ArtigoID_DD016 { get; init; }
+        public DtoGetGG006Simples? NavGG006MarcaID_DD016 { get; init; }
+        public CSICP_DD016Apl? NavDD016Aplicacao_DD016 { get; init; }
+        public DtoGetDD001SemList? NavDD001RComercializ_DD016 { get; init; }
+
         public static DtoGetDD016 FromEntity(CSICP_DD016 entity)
         {
             return new DtoGetDD016
@@ -40,7 +63,15 @@ namespace CSCore.Application.Dto.Dtos.DD.DD016
                 Dd016Aplicacao = entity.Dd016Aplicacao,
                 Dd016Isactive = entity.Dd016Isactive,
                 Dd016Protocolnumber = entity.Dd016Protocolnumber,
-                Dd001Rcomercializid = entity.Dd001Rcomercializid
+                Dd001Rcomercializid = entity.Dd001Rcomercializid,
+                NavBB001FilialID_DD016 = entity.NavBB001FilialID_DD016?.ToDtoGetExibicao(),
+                NavBB008CondicaoID_DD016 = entity.NavBB008CondicaoID_DD016?.ToDtoGetSimples(),
+                NavGG003GrupoID_DD016 = entity.NavGG003GrupoID_DD016?.ToDtoGetSimples(),
+                NavGG004ClasseID_DD016 = entity.NavGG004ClasseID_DD016?.ToDtoGetSemFilial(),
+                NavGG005ArtigoID_DD016 = entity.NavGG005ArtigoID_DD016?.ToDtoGetSemFilial(),
+                NavGG006MarcaID_DD016 = entity.NavGG006MarcaID_DD016?.ToDtoGetSimples(),
+                NavDD016Aplicacao_DD016 = entity.NavDD016Aplicacao_DD016,
+                NavDD001RComercializ_DD016 = entity.NavDD001RComercializ_DD016?.ToDtoGetDD001()
             };
         }
     }
