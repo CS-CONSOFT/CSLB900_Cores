004419d6e818a461807762f2cdd79bbb5d275408
Jardel Barros <jardelbarros@cs-consoft.com.br>
2026-02-05 15:51:07 -0300

    feat: Adiciona entidades RR009, RR010 e RR011 ao contexto EF
    Inclui as entidades OsusrTo3CsicpRr009, OsusrTo3CsicpRr010 e OsusrTo3CsicpRr011 no AppDbContext_RR.cs, com respectivos arquivos de classe e mapeamentos no OnModelCreating. Realiza pequenas limpezas de código e atualiza o submódulo CSLB900_Cores para estado "dirty".

---
+++ CSCore.Domain/CS_Models/CSICP_RR/OsusrTo3CsicpRr011.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSCore.Domain.CS_Models.CSICP_RR;
+
+public partial class OsusrTo3CsicpRr011
+{
+    public int TenantId { get; set; }
+
+    public long Id { get; set; }
+
+    public string? Rr011Serie { get; set; }
+
+    public int? Rr011Ultrgn { get; set; }
+}
