5463f9eaa2fc6efec7915f78dece94bff28e888e
Jardel Barros <jardelbarros@cs-consoft.com.br>
2026-03-04 08:42:29 -0300

    refactor: Alterar DTO de retorno em Abrir/Fechar Lote CG020
    Atualiza a propriedade RegistroAtualizado em DtoAbrirFecharLoteCG020 para usar DtoGetCG020Padrao, ajustando também o método de conversão no serviço correspondente. Inclui atualização do submódulo CSLB900_Cores com alterações não commitadas.

--- CSCore.Application.Dto/Dtos/CG/CG020/DtoAbrirFecharLoteCG020.cs
+++ CSCore.Application.Dto/Dtos/CG/CG020/DtoAbrirFecharLoteCG020.cs
@@ -4,6 +4,6 @@ namespace CSCore.Application.Dto.Dtos.CG.CG020
     {
         public string Cg020Id { get; set; } = null!;
         public string StatusLoteAtual { get; set; } = null!;
-        public DtoGetCG020 RegistroAtualizado { get; set; } = null!;
+        public DtoGetCG020Padrao RegistroAtualizado { get; set; } = null!;
     }
 }
